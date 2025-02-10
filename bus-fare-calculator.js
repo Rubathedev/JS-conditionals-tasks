ce740f0be7827b7665ab9f2316f27bf892cb7e45
dev.Ruabyet <dev.rubayethasan@gmail.com>
2025-02-10 20:12:11 +0600

    added bus fare calculator

---
+++ bus-fare-calculator.js
@@ -0,0 +1,26 @@
+/***
+
+Ticket fare Calculator
+    - Children (age < 10): free
+    - Students get a 50% discount
+    - Senior citizens (age >= 60) gets a 15% Discount
+    - Otherwise Regular ticket fare 800 tk
+*/
+let ticket = 800;
+let age = 18;
+let student ;
+
+let pay;
+if (age<10){
+    
+    console.log("your ticket is free")
+}
+else if (student == student) {
+    ticket = ticket *50 /100;
+    console.log(ticket);
+}
+else if(age >=65 ){
+    ticket = ticket *15 /100;
+    console.log(ticket);
+}
+else console.log(ticket);
